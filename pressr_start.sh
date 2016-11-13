04ea2fbd5d328691aeeb9b125aafdd514107970e
Yasser Nabi <yassersaleemi@gmail.com>
2016-11-13 05:48:41 +0000

    Remove sql update
    Save info that we don't want

--- pressr_start.sh
+++ pressr_start.sh
@@ -3,13 +3,5 @@
 set -e
 set -o pipefail
 
-MYSQL_OPTS="--connect-timeout=10 -w -u ${WORDPRESS_DB_USER} --password=${WORDPRESS_DB_PASSWORD} -h ${WORDPRESS_DB_HOST}"
-TABLE_COUNT_QUERY="SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '${WORDPRESS_DB_NAME}'"
-
-
-if [[ $(mysql -N -s $MYSQL_OPTS -e "${TABLE_COUNT_QUERY}") -eq 0 ]]
-then
-  mysql $MYSQL_OPTS -D ${WORDPRESS_DB_NAME} < /pressr.sql
-fi
 
 apache2-foreground
